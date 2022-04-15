88bc9cfaf7653d491ddc4cf6055ee59bac749d29
Raihanul islam <97145938+Raihanulislam12s@users.noreply.github.com>
2022-04-15 12:33:14 -0700

    Create rotatescreen.py

---
+++ rotatescreen.py
@@ -0,0 +1,13 @@
+#pip install rotate-screen
+#pip install pypiwin32
+
+import time
+import rotatescreen
+screen = rotatescreen.get_primary_display()
+screen.rotate_to(0)
+for i in range(10):
+    time.sleep(1)
+    screen.rotate_to(i*90%360)
+
+    #stop this problems fass ctrl + F2
+
